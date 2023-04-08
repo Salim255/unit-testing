0bbbd1e793c43bc2d7cb41840d88f53123bf23eb
Salim <salim.hassan.welleat@gmail.com>
2023-04-08 14:10:37 +0200

    Introducing Test Suites

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,33 +1,35 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { transformToNumber } from "./numbers";
 
-it("should transform a string number to a number of type number,", () => {
-  //Arrange
-  const str = "1";
-  //Act
-  const result = transformToNumber(str);
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number,", () => {
+    //Arrange
+    const str = "1";
+    //Act
+    const result = transformToNumber(str);
 
-  //Expected
-  expect(result).toBeTypeOf("number");
-});
+    //Expected
+    expect(result).toBeTypeOf("number");
+  });
 
-it("should transform a string number to a number of type number,", () => {
-  //Arrange
-  const str = "1";
-  //Act
-  const result = transformToNumber(str);
+  it("should transform a string number to a number of type number,", () => {
+    //Arrange
+    const str = "1";
+    //Act
+    const result = transformToNumber(str);
 
-  //Expected
-  expect(result).toBe(+str);
-});
+    //Expected
+    expect(result).toBe(+str);
+  });
 
-it("should yield NaN for non-transformable values", () => {
-  //Arrange
-  const str = "invalid";
-  const str2 = {};
-  //Acte
-  const result = transformToNumber(str);
-  const result2 = transformToNumber(str2);
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+  it("should yield NaN for non-transformable values", () => {
+    //Arrange
+    const str = "invalid";
+    const str2 = {};
+    //Acte
+    const result = transformToNumber(str);
+    const result2 = transformToNumber(str2);
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
